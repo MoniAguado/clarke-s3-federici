25916c6d8e782aba1496f2b7cdcadcba43f9054c
Adalaber <maguado85@gmail.com>
2018-01-03 12:00:33 +0100

    fix display flex + js burger don't finish

--- js/main.js
+++ js/main.js
@@ -1,1 +1,13 @@
 'use strict';
+
+/*transición del burger*/
+var burger = document.querySelector('.button-burger');
+var desplegable = document.querySelector('.menu');
+
+function addClassMenu(){
+	burger.classList.remove('hidden');
+	desplegable.classList.add('new-position');
+	/*falta que se esconda de nuevo*/
+}
+
+burger.addEventListener('click', addClassMenu);
