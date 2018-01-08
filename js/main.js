d15c200cd5e3932a7088fd4202116db9c37037e4
Adalaber <maguado85@gmail.com>
2018-01-08 12:18:16 +0100

    menu-burger

--- js/main.js
+++ js/main.js
@@ -1,24 +1,33 @@
 'use strict';
 
 // efecto transicion burger menu-x
-document.querySelector( "#burger" )
-  .addEventListener( "click", function() {
+document.querySelector("#burger")
+  .addEventListener("click", function() {
     this.classList.toggle( "active" );
   });
 
+var burger = document.querySelector('.burger');
+var menu = document.querySelector('.menu');
+function showMenu (){
+	// menu.classList.toggle('hidden');
+	menu.classList.toggle('new-position');
 
-// desplegable burger
-var burger = document.querySelectorAll('.burger');
-function menu(event){
-	var burgerId = event.currentTarget.getAttribute('burger');
-	var desplegable = document.getElementById(burgerId);
-	if (desplegable.style.display == 'block'){
-		desplegable.style.display = 'none';
-	} else {
-		desplegable.style.display = 'block';
-	}
 }
 
-burger.addEventListener('click', menu);
+burger.addEventListener('click', showMenu);
+
+
+// desplegable burger
+// function menu(event){
+// 	var burgerId = event.currentTarget.getAttribute('burger');
+// 	var desplegable = document.getElementById(burgerId);
+// 	if (desplegable.style.display == 'block'){
+// 		desplegable.style.display = 'none';
+// 	} else {
+// 		desplegable.style.display = 'block';
+// 	}
+// }
+//
+// burger.addEventListener('click', menu);
 
 // burger.addEventListener('click', menu);
