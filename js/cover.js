45f65a32f23f45b233688296849f4a032c287c0b
LauraNavRuso <laura.nav.ruso@gmail.com>
2018-01-13 20:41:12 +0100

    add images-bcg-button

--- js/cover.js
+++ js/cover.js
@@ -4,12 +4,10 @@ function flip() {
 	document.getElementById('card').classList.toggle('flipped');
 }
 
-// function appearButton() {
-// 	document.querySelector('.button--cover').classList.toggle('button--cover-visible');
-//
-// 	// document.querySelector('.button--cover').classList.remove('button--cover-invisible');
-// 	// document.querySelector('.button--cover').classList.add('button--cover-visible');
-// }
+function appearButton() {
+	document.querySelector('.button--cover').classList.remove('button--cover-invisible');
+	document.querySelector('.button--cover').classList.add('bigEntrance');
+}
 
 setTimeout(flip, 3000);
-// setTimeout(appearButton, 3000);
+setTimeout(appearButton, 3500);
