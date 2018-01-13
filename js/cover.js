ccf73363e71f368ec2574f0a6070ebb0378cdaa3
LauraNavRuso <laura.nav.ruso@gmail.com>
2018-01-13 18:09:03 +0100

    add card-effect working with rectangle and siteTimeout

---
+++ js/cover.js
@@ -0,0 +1,15 @@
+'use strict';
+
+function flip() {
+	document.getElementById('card').classList.toggle('flipped');
+}
+
+// function appearButton() {
+// 	document.querySelector('.button--cover').classList.toggle('button--cover-visible');
+//
+// 	// document.querySelector('.button--cover').classList.remove('button--cover-invisible');
+// 	// document.querySelector('.button--cover').classList.add('button--cover-visible');
+// }
+
+setTimeout(flip, 3000);
+// setTimeout(appearButton, 3000);
