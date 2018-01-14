e8ae7c7c5bd6916e788725d480e21238d979510f
LauraNavRuso <laura.nav.ruso@gmail.com>
2018-01-14 12:41:45 +0100

    save before new branch

--- js/quiz.js
+++ js/quiz.js
@@ -4,17 +4,30 @@
 var feedbackTextWin = '¡Acertaste!';
 var feedbackTextLose = '¡Fallaste!';
 
+
+
+
 function openResultBox() {
-	document.querySelector('.result-box').classList.remove('result-box-invisible');
+	var resultBox = 	document.querySelectorAll('.result-box');
+
+	for (var j = 0; j < resultBox.length; j++) {
+		resultBox[j].classList.remove('result-box-invisible');
+	}
 }
 
 function closeIconsToResponseAndOpenResultBox(event) {
+
+
+
 	document.querySelector('.icons-to-response').classList.add('scale-out-horizontal');
 
 	if (event.currentTarget.classList.contains('correct-answer')) {
 			document.querySelector('.feedback').insertAdjacentHTML('beforeend', feedbackTextWin);
+
 			document.querySelector('.feedback').classList.add('feedbackWin');
+
 			document.querySelector('.icon-container').classList.add('icon-container-right');
+
 		} else {
 			document.querySelector('.feedback').insertAdjacentHTML('beforeend', feedbackTextLose);
 			document.querySelector('.feedback').classList.add('feedbackLose');
@@ -30,6 +43,37 @@ for (var i = 0; i < pressedEmoji.length; i++) {
 	pressedEmoji[i].addEventListener('click', closeIconsToResponseAndOpenResultBox);
 }
 
+
+
+
+// var feedbackTextWin = '¡Acertaste!';
+// var feedbackTextLose = '¡Fallaste!';
+//
+// function openResultBox() {
+// 	document.querySelector('.result-box').classList.remove('result-box-invisible');
+// }
+//
+// function closeIconsToResponseAndOpenResultBox(event) {
+// 	document.querySelector('.icons-to-response').classList.add('scale-out-horizontal');
+//
+// 	if (event.currentTarget.classList.contains('correct-answer')) {
+// 			document.querySelector('.feedback').insertAdjacentHTML('beforeend', feedbackTextWin);
+// 			document.querySelector('.feedback').classList.add('feedbackWin');
+// 			document.querySelector('.icon-container').classList.add('icon-container-right');
+// 		} else {
+// 			document.querySelector('.feedback').insertAdjacentHTML('beforeend', feedbackTextLose);
+// 			document.querySelector('.feedback').classList.add('feedbackLose');
+// 			document.querySelector('.icon-container').classList.add('icon-container-wrong');
+// 		}
+//
+// 	setTimeout(openResultBox, 1500)
+// }
+//
+// var pressedEmoji = document.querySelectorAll('.emoji');
+//
+// for (var i = 0; i < pressedEmoji.length; i++) {
+// 	pressedEmoji[i].addEventListener('click', closeIconsToResponseAndOpenResultBox);
+// }
 
 // 2. FUNCIONALIDAD REPRODUCIR AUDIOS__________________
 function playDespacito() {
