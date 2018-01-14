b217d9184d4d5c7804182a194fea31438f632d75
LauraNavRuso <laura.nav.ruso@gmail.com>
2018-01-14 09:40:02 +0100

    complete response styles

--- js/quiz.js
+++ js/quiz.js
@@ -3,17 +3,24 @@
 var feedbackTextWin = '¡Acertaste!';
 var feedbackTextLose = '¡Fallaste!';
 
-function closeIconsToResponseAndOpenResultBox(event) {
-	document.querySelector('.icons-to-response').classList.add('icons-to-response-invisible');
+function openResultBox() {
 	document.querySelector('.result-box').classList.remove('result-box-invisible');
+}
+
+function closeIconsToResponseAndOpenResultBox(event) {
+	document.querySelector('.icons-to-response').classList.add('scale-out-horizontal');
 
 	if (event.currentTarget.classList.contains('correct-answer')) {
-		document.querySelector('.feedback').insertAdjacentHTML('beforeend', feedbackTextWin);
-		document.querySelector('.feedback').classList.add('feedbackWin');
-	} else {
-		document.querySelector('.feedback').insertAdjacentHTML('beforeend', feedbackTextLose);
-		document.querySelector('.feedback').classList.add('feedbackLose');
-	}
+			document.querySelector('.feedback').insertAdjacentHTML('beforeend', feedbackTextWin);
+			document.querySelector('.feedback').classList.add('feedbackWin');
+			document.querySelector('.icon-container').classList.add('icon-container-right');
+		} else {
+			document.querySelector('.feedback').insertAdjacentHTML('beforeend', feedbackTextLose);
+			document.querySelector('.feedback').classList.add('feedbackLose');
+			document.querySelector('.icon-container').classList.add('icon-container-wrong');
+		}
+
+	setTimeout(openResultBox, 1500)
 }
 
 var pressedEmoji = document.querySelectorAll('.emoji');
